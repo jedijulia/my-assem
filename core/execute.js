daa9b961db2c3c1de6a8a0d14838b72b366041f7
Kiel Labian <kiellabian@gmail.com>
2015-04-19 14:17:38 +0800

    merged branch executer

--- core/execute.js
+++ core/execute.js
@@ -1,26 +1,24 @@
-read*
-disp*
-pushi*
-pushv*
-pop*
-mod*
-jmp*
-jl
-jg
-jeq
-add
-sub
-cmp
-begin
-end
-
-
 function execute(memory) {
     var run = true;
     var error = null;
     var program_counter = 0;
 
     var stack = [];
+    var max_stack_size = 5;
+
+    while(run) {
+        var command = memory[program_counter];
+        var code = command.substring(0, 2);
+        var params = command.substring(2);
+        var prev_pc = program_counter;
+        this[resolve_code_to_name(code)](params);
+        if (program_counter == prev_pc) {
+            program_counter += 1;
+        }
+        if (error != null) {
+            alert(error);
+        }
+    }
 
     function resolve_code_to_name(code) {
         // should return the string name of the function corresponding to the
@@ -38,7 +36,7 @@ function execute(memory) {
     function read(params) {
         var input = prompt("Enter a number");
         var num = parseInt(input)
-        if (input.length > 2 || input == NaN) {
+        if (input.length > 2 || input === NaN) {
             error = "Invalid input.";
             run = false;
             return;
@@ -47,7 +45,7 @@ function execute(memory) {
     }
 
     function disp(params) {
-        if (memory[params] == undefined) {
+        if (memory[params] === undefined) {
             error = "Trying to access variable that is not defined!"
             run = false;
             return;
@@ -55,8 +53,58 @@ function execute(memory) {
         alert(memory[params]);
     }
 
+    function pushi(params) {
+        if (stack.length === max_stack_size) {
+            error = "Stack Overflow!"
+            run = false;
+            return;
+        }
+        stack.push(params)
+    }
 
+    function pushv(params) {
+        if (memory[params] === undefined) {
+            error = "Trying to access variable that is not defined!"
+            run = false;
+            return;
+        }
+        if (stack.length === max_stack_size) {
+            error = "Stack Overflow!"
+            run = false;
+            return;
+        }
+        stack.push(memory[params]);
+    }
 
+    function pop(params) {
+        if (stack.length === 0) {
+            error = "Stack Overflow!"
+            run = false;
+            return;
+        }
+        var num = stack.pop();
+        memory[params] = num;
+    }
+
+    function mod(params) {
+        var stack_size = stack.length;
+        if (stack_size < 2) {
+            error = "Null Operand!"
+            run = false;
+            return;
+        }
+        if (stack_size === max_stack_size) {
+            error = "Stack Overflow!"
+            run = false;
+            return;
+        }
+        var m = stack[stack_size - 2] % stack[stack_size - 1]
+        stack.push(m);
+    }
+
+    function jmp(params) {
+        program_counter = params;
+    }
 
     function jl(params) {
         var l = stack.length;

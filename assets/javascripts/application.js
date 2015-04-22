1f686929cd98fccb354eb12a5d8c0b823bb0855e
Arnelle Balane <arnellebalane@gmail.com>
2015-04-22 21:49:16 +0800

    integrate translator to code editor

---
+++ assets/javascripts/application.js
@@ -0,0 +1,27 @@
+var menu = {
+    file: function() {
+        console.info('FILE');
+    },
+    translate: function() {
+        var code = $('textarea').val().trim();
+        code = code.replace(/(\r?\n)+/g, '\n').replace(/ +/g, ' ');
+        code = code.split(/\r?\n/g);
+
+        var translated = translate(code);
+        var container = $('#translation ul').empty();
+        for (var i = 0, l = translated.translation.length; i < l; i++) {
+            container.append('<li>' + translated.translation[i] + '</li>');
+        }
+    },
+    trace: function() {
+        console.info('TRACE');
+    },
+    execute: function() {
+        console.info('EXECUTE');
+    }
+};
+
+
+$('nav').on('click', 'a[data-action]', function() {
+    menu[$(this).data('action')]();
+});
